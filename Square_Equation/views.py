5f65b940f7ea848e5339d649ebc9ccadd9983310
KrivovDE <55reigen55@mail.ru>
2022-03-18 12:37:26 +0300

    transformation views and html

--- Square_Equation/views.py
+++ Square_Equation/views.py
@@ -8,19 +8,27 @@ def start(request):
 
 
 def finish(request):
+    """Преобразуем днные полученные от пользователя в переенные питона"""
     a = float(request.GET.get('a'))
     b = float(request.GET.get('b'))
     c = float(request.GET.get('c'))
 
-    D = b ** 2 - 4 * a * c
-    if D > 0:
-        x1 = (-b + math.sqrt(D)) / (2 * a)
-        x2 = (-b - math.sqrt(D)) / (2 * a)
-        return render(request, 'finish.html', {'a': a, 'b': b, 'c': c, 'D': D, 'x1': x1, 'x2': x2})
-    elif D == 0:
-        x3 = (-b + math.sqrt(D)) / (2 * a)
-        return render(request, 'finish.html', {'a': a, 'b': b, 'c': c, 'D': D, 'x3': x3})
+    """Проверяем что а != 0, иначе будет ошибка деления на 0"""
+    if a == 0:
+        x = 'Уравнение не имеет решений, так как возникает ошибка "Zero Division Error",' \
+            ' убедитесь что введенное значение "а" не равно 0'
+        return render(request, 'finish.html', {'a': a, 'b': b, 'c': c, 'x': x})
     else:
-        x4 = 'Уравнение не имеет решений'
-        return render(request, 'finish.html', {'a': a, 'b': b, 'c': c, 'D': D, 'x4': x4})
+        """Считаем дискриминант и в зависимости от этого, применяем формулу"""
+        D = b ** 2 - 4 * a * c
+        if D > 0:
+            x1 = (-b + math.sqrt(D)) / (2 * a)
+            x2 = (-b - math.sqrt(D)) / (2 * a)
+            return render(request, 'finish.html', {'a': a, 'b': b, 'c': c, 'D': D, 'x1': x1, 'x2': x2})
+        elif D == 0:
+            x3 = (-b + math.sqrt(D)) / (2 * a)
+            return render(request, 'finish.html', {'a': a, 'b': b, 'c': c, 'D': D, 'x3': x3})
+        else:
+            x4 = 'Уравнение не имеет решений'
+            return render(request, 'finish.html', {'a': a, 'b': b, 'c': c, 'D': D, 'x4': x4})
 
